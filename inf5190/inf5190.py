febd5fa31b839b9623bad75500f07ad4cf56516f
Noura Djaffri <nouradjaffri@gmail.com>
2019-10-04 17:28:34 -0400

    removed package requests to use urllib

--- inf5190/inf5190.py
+++ inf5190/inf5190.py
@@ -1,19 +1,25 @@
 from flask import Flask, jsonify
-import requests
 import json
+from urllib.error import URLError, HTTPError
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
 @app.route('/',  methods=['GET'])
 def index():
-    uri = " https://caissy.dev/shops/products"
-    try : 
-        reponse = requests.get(uri)
-    except requests.ConnectionError:
-        return "Connection Error"
-    reponseJson = reponse.text
-    data = json.loads(reponseJson)
-    return data 
+    url = "https://caissy.dev/shops/products"
+    try:
+        response = urlopen(url).read()
+    except HTTPError as e:
+        print("Error code : ", e.code)
+    except URLError as e:
+        print("Error code : ", e.reason) #load a jSON file with the error
+
+    return json.loads(str(response, encoding='utf-8'))
+
+@app.route('/order/<int:order_id>', methods=['POST'])
+def order_product(order_id):
+    return "Order" #return code 302 + link vers la commande nouvellement crée
 
 
 if __name__ == '__main__':
