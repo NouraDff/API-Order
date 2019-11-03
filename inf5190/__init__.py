ed6482d4f3c0a05c70aae8044dc40599ac954945
Noura Djaffri <nouradjaffri@gmail.com>
2019-11-02 23:20:27 -0400

    Shipping info & payment

--- inf5190/__init__.py
+++ inf5190/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request, redirect, Response
 import json
 from urllib.error import URLError, HTTPError
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+from urllib import parse
 import logging
 from inf5190.models import init_app, Product, Order
 from functools import lru_cache
@@ -77,38 +78,76 @@ def create_app(initial_config=None):
     
 
 
-    @app.route('/order/<int:order_id>', methods=['GET', 'PUT'])
-    def get_product(order_id):
+    @app.route('/order/<int:order_id>', methods=['GET'])
+    def get_order(order_id):
         if request.method == 'GET':
-            order = Order.select().where(Order.id == order_id).get()
-            order = model_to_dict(order)
+            order_bd = Order.select().where(Order.id == order_id).get()
+            order = model_to_dict(order_bd)
             
             product_id = order['product']['id']
             product = Product.select().where(Product.id == product_id).get() 
             product = model_to_dict(product)
 
-            weight = product['weight']
+            weight = product['weight'] * order['product']['quantity']
             if(weight <= 500):
                 order['shipping_price'] = 500
             elif(weight <= 2000):
                 order['shipping_price'] = 1000
             else:
                 order['shipping_price'] = 2500
-          
-        if request.method == 'PUT': 
-            query = Order.update(request.json['order']).where(Order.id == order_id)
-            query.execute()
-            order = Order.select().where(Order.id == order_id).get()
-            order = model_to_dict(order)
-             
+
+            order_bd.shipping_price =  order['shipping_price']
+            order_bd.save()
+
             
-
-
         dict_order = {}
         dict_order['order'] = order
 
         return dict_order
+
+
+    @app.route('/order/<int:order_id>', methods=['PUT'])
+    def edit_order(order_id):
+        
+        if 'order' in request.json:
+            query = Order.update(request.json['order']).where(Order.id == order_id)
+            query.execute()
+            order = Order.select().where(Order.id == order_id).get()
+            order = model_to_dict(order)   
+
+            dict_order = {}
+            dict_order['order'] = order
+            return dict_order
+
+        elif 'credit_card' in request.json:
+            order_db = Order.select().where(Order.id == order_id).get()
+            order = model_to_dict(order_db) 
+            total_amount = order['shipping_price'] + order['total_price']
+
+            request.json["amount_charged"] = total_amount
     
+            req = Request("https://caissy.dev/shops/pay", headers={'Host' : 'caissy.dev', 'Content-Type':'application/json'} , method='POST')
+            data = json.dumps(request.json).encode("utf8")
+            res = urlopen(req, data).read()
+            confirmation = res.decode('utf8')
+            confirmation = json.loads(confirmation)
+            if confirmation['transaction']['success'] == True :
+                order_db.credit_card = confirmation['credit_card']
+                order_db.transaction = confirmation['transaction']
+                order_db.paid = True
+                order_db.save()
+            
+            order = model_to_dict(order_db)   
+            dict_order = {}
+            dict_order['order'] = order
+            return dict_order
+            
+
+            #TODO check if all required info are there
+            
+
+        
+
     
     return app
 
